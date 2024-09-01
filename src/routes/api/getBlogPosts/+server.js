b7b2b46d820b7c59cbdd5db0f92b42710a738d79
Nicko-89 <52646404+Nicko-89@users.noreply.github.com>
2024-09-01 15:24:15 +1000

    Update +server.js

--- src/routes/api/getBlogPosts/+server.js
+++ src/routes/api/getBlogPosts/+server.js
@@ -1,21 +1,11 @@
-import contentful from 'contentful';
-import { json, error } from '@sveltejs/kit';
+const contentful = require('contentful')
 
-export async function GET() {
-    if(!import.meta.env.VITE_CONTENTFUL_CLIENT_ACCESS_TOKEN) {
-        throw error(500, "Missing VITE_CONTENTFUL_CLIENT_ACCESS_TOKEN (added dependency in v2.0), go to https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#iii-add-a-blog for directions to add it");
-    }
-    const client = contentful.createClient({
-        // This is the space ID. A space is like a project folder in Contentful terms
-        space: import.meta.env.VITE_CONTENTFUL_SPACE,
-        // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-        accessToken: import.meta.env.VITE_CONTENTFUL_CLIENT_ACCESS_TOKEN
-    });
-	const data = await client.getEntries({content_type: 'blog_post'})
-        .catch(e=> {
-            console.error(e);
-            throw error(500, "Problem retrieving blog posts");
-        });
+const client = contentful.createClient({
+  space: 'vcqjjufn470k',
+  environment: 'master', // defaults to 'master' if not set
+  accessToken: 'HGEFEEB-9ZVueLQyMvLtCmfLjX0sD25aSohhVse4yb0'
+})
 
-    return json(data);
-}
+client.getEntry('12rKAi3rlgKk5VDDlULZrX')
+  .then((entry) => console.log(entry))
+  .catch(console.error)
